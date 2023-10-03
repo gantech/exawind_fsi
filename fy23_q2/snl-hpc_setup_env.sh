337b848af1a9aa678b8fd50626b94161beb31906
Philip Sakievich <psakiev@sandia.gov>
2023-10-03 14:57:59 -0600

    Update SNL stuff

--- fy23_q2/snl-hpc_setup_env.sh
+++ fy23_q2/snl-hpc_setup_env.sh
@@ -1,2 +1,3 @@
-# nothing yet...
-# We need a module that will load aprepro and the exawind executables
+
+module use /projects/wind/spack-manager/modules
+module load linux-rhel7-skylake_avx512/hfm-fsi/exawind-intel/2021.3.0
