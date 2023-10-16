e4b791bc6cda315d2e2bdc88b7c0720ebd01f482
Philip Sakievich <psakiev@sandia.gov>
2023-10-16 16:18:47 -0600

    Smart selection of executable on snl-hpc

--- fy23_q2/snl-hpc_setup_env.sh
+++ fy23_q2/snl-hpc_setup_env.sh
@@ -1,2 +1,6 @@
 module use /projects/wind/spack-manager/modules
+if [[ "${SNLCLUSTER}" == "attaway" ]]; then 
 module load linux-rhel7-skylake_avx512/hfm-fsi/exawind-intel-2021.3.0
+else
+module load linux-rhel7-x86_64/hfm-fsi/exawind-intel-2021.3.0
+fi
