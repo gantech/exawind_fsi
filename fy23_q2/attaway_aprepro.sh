324933ac17c349dea41e7a7be39e21117768561e
Philip Sakievich <psakiev@sandia.gov>
2023-10-19 10:15:09 -0600

    Update meshes and lower beam order

--- fy23_q2/attaway_aprepro.sh
+++ fy23_q2/attaway_aprepro.sh
@@ -6,7 +6,7 @@
 
 {MACHINE_NAME="attaway"}
 {NALU_RANK_PERCENTAGE=25}
-{MESH="/projects/wind/meshes/split/iea15mw/iea15mw_3blade_volume_v0.1.exo"}
+{MESH="/projects/wind/meshes/split/iea15mw/iea15mw_3blade_volume_v0.2.exo"}
 {DLL="/projects/wind/spack-manager/environments/hfm-fsi/opt/linux-rhel7-skylake_avx512/intel-2021.3.0/rosco-2.7.0-tyghuah43gqtdef6cskyksnq2uktlcfq/lib/libdiscon.so"}
 
 {CLEANUP_ARGS = '
