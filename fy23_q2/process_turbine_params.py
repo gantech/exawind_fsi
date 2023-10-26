621d4d4daf1eb2e2c85153ca62c918da2ced2c54
Nathaniel deVelder <ndeveld@sandia.gov>
2023-10-25 21:05:12 -0600

    Fixes to scripting including ratio truncation

--- fy23_q2/process_turbine_params.py
+++ fy23_q2/process_turbine_params.py
@@ -4,6 +4,9 @@ import csv
 import sys
 import math
 
+def truncate(f, n):
+    return math.floor(f * 10 ** n) / 10 ** n
+
 def linear_interpolation(param, data):
     # Sort the data by the first column
     data.sort(key=lambda row: row[0])
@@ -22,23 +25,26 @@ def linear_interpolation(param, data):
     return interpolated_rpm, interpolated_pitch
 
 def calc_timesteps(rpm,prec_len):
-    rps = rpm / 60.0
-    rotdeg = rps * 360.0
-    rotrad = rps * 2.0 * math.pi
-    cfd_dt = 0.25/rotdeg
+    revps = rpm / 60.0
+    degps = revps * 360.0
+    radps = revps * 2.0 * math.pi
+
+    cfd_dt = 0.25/degps
     # ratio of 100 matches tests by neil for the 10.59 m/s case
     # using 120 just to be safe for higher wind speeds
-    ratio = int(3*math.ceil(rotdeg) - 30)
+    ratio = int(3*math.ceil(degps) - 30)
     openfast_dt = cfd_dt / ratio
 
-    prec_revs = int(math.floor(prec_len * rps))
-    prec_chkp_length = prec_revs / rps
-    prec_chkp_n = int(prec_chkp_length / cfd_dt)
+    prec_revs = int(math.floor(prec_len * revps))
+    prec_chkp_length = prec_revs / revps
     
-    precursor_rad = prec_chkp_length * rotrad
+    prec_chkp_cfd_n = int(prec_chkp_length / cfd_dt)
+    prec_chkp_of_n = int(prec_chkp_length / openfast_dt)
+
+    precursor_rad = prec_chkp_length * radps
     az_blend_mean = precursor_rad + 3.0 * 2.0 * math.pi
 
-    return cfd_dt, openfast_dt, prec_chkp_length, az_blend_mean
+    return truncate(cfd_dt,15), truncate(openfast_dt,15), truncate(prec_chkp_length,15), truncate(az_blend_mean,15)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -57,5 +63,9 @@ if __name__ == "__main__":
            data.append((speed, rpm, pitch))
        designRPM, designPitch = linear_interpolation(wind_speed, data)
        cfdDt, openfastDt, precLen, azBlend = calc_timesteps(designRPM, precursor_length)
-    print(designRPM, designPitch, f"{cfdDt:.17f}", f"{openfastDt:.17f}", f"{azBlend:.17f}", f"{precLen:.17f}" , int(cfdDt/openfastDt), int(precLen/openfastDt))
+       ratio = cfdDt/openfastDt
+       int_ratio = int(ratio)
+       assert ratio >= float(int_ratio)
+       assert int(ratio) == int_ratio    
+    print(designRPM, designPitch, f"{cfdDt:.15f}", f"{openfastDt:.15f}", f"{azBlend:.15f}", f"{precLen:.15f}" , cfdDt/openfastDt, int(precLen/openfastDt))
 
