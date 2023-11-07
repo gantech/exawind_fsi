d9535513bf621d2f4c2ba6a39e80ba5bd48ab317
Nathaniel deVelder <ndeveld@sandia.gov>
2023-11-07 12:59:58 -0700

    Fixes to hub pos, modules, and aerodyn visc

--- fy23_q2/amber_setup_env.sh
+++ fy23_q2/amber_setup_env.sh
@@ -1,6 +1,6 @@
 module use /projects/wind/spack-manager/modules
-module load linux-rhel8-icelake/hfm-fsi/exawind-intel-2021.3.0  
-module load linux-rhel8-icelake/hfm-fsi/rosco-intel-2021.3.0
+module load linux-rhel8-icelake/hfm-fsi/exawind-intel-2023.2.0  
+module load linux-rhel8-icelake/hfm-fsi/rosco-intel-2023.2.0
 
 umask 007
 function ranks_per_node(){
