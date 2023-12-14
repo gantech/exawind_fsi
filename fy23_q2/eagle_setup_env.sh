33888087245620166140002a6157ec600914e594
kevmoor <kevmoor@sandia.gov>
2023-12-14 12:44:17 -0700

    Update module load for avalaible, decrease time for initial test submit

--- fy23_q2/eagle_setup_env.sh
+++ fy23_q2/eagle_setup_env.sh
@@ -1,5 +1,5 @@
 module use /projects/hfm/kmoore/spack-manager/modules
-module load hfm_fsi_fy23/exawind-gcc-9.3.0
+module load gcc/9.3.0
 function ranks_per_node(){
   lscpu | grep -m 1 "CPU(s):" | awk '{print $2}'
 }
