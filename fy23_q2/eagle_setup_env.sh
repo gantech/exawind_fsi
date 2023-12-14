7628b210fca6dff4299be3c10740a09cbcc1d256
kevmoor <kevmoor@sandia.gov>
2023-12-14 12:34:38 -0700

    updates for a test submission

--- fy23_q2/eagle_setup_env.sh
+++ fy23_q2/eagle_setup_env.sh
@@ -1,4 +1,4 @@
-module use /projects/exawind/psakiev/spack-manager/modules
+module use /projects/hfm/kmoore/spack-manager/modules
 module load hfm_fsi_fy23/exawind-gcc-9.3.0
 function ranks_per_node(){
   lscpu | grep -m 1 "CPU(s):" | awk '{print $2}'
