dc7eb989254e98b93831b4bc4822f7556a089150
kevmoor <kevmoor@sandia.gov>
2023-12-14 15:20:05 -0700

    update

--- fy23_q2/eagle_setup_env.sh
+++ fy23_q2/eagle_setup_env.sh
@@ -1,5 +1,13 @@
-module use /projects/hfm/kmoore/spack-manager/modules
 module load gcc/9.3.0
+modul load mpt/2.22
+export SPACK_MANAGER=/projects/hfm/kmoore/spack-manager
+source $SPACK_MANAGER/start.sh
+spack-start
+spack env activate /projects/hfm/kmoore/spack-manager/environments/exawind
+spack load nalu-wind
+spack load amr-wind
+spack load openfast
+
 function ranks_per_node(){
   lscpu | grep -m 1 "CPU(s):" | awk '{print $2}'
 }
