f86ab224cbae68dbdd2ee55971c1a942b709980e
Kevin Moore <kmoore@el2.ib0.cm.hpc.nrel.gov>
2024-01-19 14:59:13 -0700

    Update to actually use the system build

--- fy23_q2/eagle_setup_env.sh
+++ fy23_q2/eagle_setup_env.sh
@@ -1,14 +1,6 @@
-module load gcc/9.3.0
-module load mpt/2.22
-export SPACK_MANAGER=/projects/hfm/kmoore/spack-manager
-source $SPACK_MANAGER/start.sh
-spack-start
-spack env activate /projects/hfm/kmoore/spack-manager/environments/exawindDev4
-spack load exawind
-spack load rosco
-spack load nalu-wind
-spack load amr-wind
-spack load openfast
+source /nopt/nrel/ecom/hpacf/env.sh # if you aren't already doing this
+module use /projects/hfm/psakiev/spack-manager/modules
+module load exawind-master/2024-01-16
 
 function ranks_per_node(){
   lscpu | grep -m 1 "CPU(s):" | awk '{print $2}'
