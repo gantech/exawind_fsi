ece82e9d37f2d4b00141a616374c2a2a05244f4e
kevmoor <kevmoor@sandia.gov>
2024-02-22 17:27:22 -0700

    Update build environment

--- fy23_q2/eagle_setup_env.sh
+++ fy23_q2/eagle_setup_env.sh
@@ -1,6 +1,6 @@
 source /nopt/nrel/ecom/hpacf/env.sh # if you aren't already doing this
 module use /projects/hfm/psakiev/spack-manager/modules
-module load exawind-master/2024-01-16
+module load exawind-master/2024-02-20
 
 function ranks_per_node(){
   lscpu | grep -m 1 "CPU(s):" | awk '{print $2}'
